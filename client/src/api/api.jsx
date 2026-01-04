12ec1beb7e6f025fdcd0baa3ceece4bd644ec53a
Sneha N <snehan102@gmail.com>
2026-01-05 03:37:50 +0530

    fix

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://personalized-news-digest-o6aa.vercel.app/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((req) => {
